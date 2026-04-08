09c161f25c79345454332838ff2a3547ea0bb9fb
sondhirania <sondhirania@gmail.com>
2026-04-08 22:35:40 +0530

    Update Timetable.js

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -4,11 +4,19 @@ function Timetable() {
       <h1>Timetable</h1>
 
       <div className="box">
-        <p>Monday: MPMC LAB, CAO TH, DAA TH, QS TH, P&S TH</p>
-        <p>Tuesday: DAA LAB, P&S TH, TOC TH, MPMC TH, WEB PROG. TH</p>
-        <p>Wednesday: WEB PROG. LAB, MPMC TH, CAO TH, DAA TH, QS TH</p>
-        <p>Thursday: QS TH, P&S TH, TOC TH</p>
-        <p>Friday: P&S LAB, WEB PROG. LAB, TOC TH, MPMC TH, CAO TH, DAA TH</p>
+        <p><b>Monday:</b> MPMC LAB, CAO TH, DAA TH, QS TH, P&S TH</p>
+        <hr />
+
+        <p><b>Tuesday:</b> DAA LAB, P&S TH, TOC TH, MPMC TH, WEB PROG TH</p>
+        <hr />
+
+        <p><b>Wednesday:</b> WEB PROG LAB, MPMC TH, CAO TH, DAA TH, QS TH</p>
+        <hr />
+
+        <p><b>Thursday:</b> QS TH, P&S TH, TOC TH</p>
+        <hr />
+
+        <p><b>Friday:</b> P&S LAB, WEB PROG LAB, TOC TH, MPMC TH, CAO TH, DAA TH</p>
       </div>
     </div>
   );
